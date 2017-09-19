f37a385a510b8400122f947c1a3ec47590e235c1
Thomas Orr <tjorr2@dcl-l440-13.ews.illinois.edu>
2017-09-18 19:33:55 -0500

    removed commented code and print statements

--- sol_1.2.3.py
+++ sol_1.2.3.py
@@ -19,7 +19,6 @@ def getURLContent(url, debug = False):
 if __name__ == "__main__":
 	ct_file = open("1.2.3_ciphertext.hex")
 	ct = ct_file.read().strip()
-	#ct = ct_pre.decode('hex')
 	result = ""
 	IV = ct[:32]
 	ct = ct[32:]
@@ -36,26 +35,17 @@ if __name__ == "__main__":
 				attempt = "0" * (32-len(hex(previous ^ test ^ padding)[2:len(hex(previous ^ test^padding))-1])) + hex(previous ^ test ^ padding)[2:len(hex(previous ^ test^padding))-1] + ct[i*32:i*32+32]
 			else:
 				attempt = "0" * (32-len(hex(previous ^ test ^ padding)[2:])) + hex(previous ^ test ^ padding)[2:] + ct[i*32:i*32+32]
-			while getURLContent("http://192.17.90.133:9999/mp1/tjorr2/?" + attempt, True) == False:
+			while getURLContent("http://192.17.90.133:9999/mp1/tjorr2/?" + attempt) == False:
 				test = test + increment
 				if hex(previous ^ test ^ padding)[len(hex(previous ^ test ^ padding))-1:] == "L":
 					attempt = "0" * (32-len(hex(previous ^ test ^ padding)[2:len(hex(previous ^ test^padding))-1])) + hex(previous ^ test ^ padding)[2:len(hex(previous ^ test^padding))-1] + ct[i*32:i*32+32]
 				else:
 					attempt = "0" * (32-len(hex(previous ^ test ^ padding)[2:])) + hex(previous ^ test ^ padding)[2:] + ct[i*32:i*32+32]
-				print attempt, len(attempt)
-				print hex(previous), len(hex(previous))
-				print hex(test), len(hex(test))
-				print hex(padding), len(hex(padding))
 			increment = increment << 8
 			current = current - 1
 			padding = (padding << 8) + current
-		print hex(test), len(hex(test))
-		print hex(padding>>8), len(hex(padding>>8))
-		print hex(test ^ (padding>>8)), len(hex(test^(padding>>8)))
-		#dummy = raw_input()
 		result = result + hex(test)[2:len(hex(test))-1]
 		previous = int(ct[i*32:i*32+32],16)
-	print result
 	print result.decode('hex')
 
 
